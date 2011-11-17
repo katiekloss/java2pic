85afe3b317c6ebb57d4aa80596498c5da3917f50
Katie Kloss <kloss.katherine@gmail.com>
2011-11-17 11:35:53 -0500

    Add argument parser

--- main.c
+++ main.c
@@ -4,13 +4,51 @@
 #include "codegen_pic.h"
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
     // process flags here
 
     ImdtCode *bytecode;
-    if(yyin = fopen(argv[1], "r"))
+    int optimize_level = 1;
+    char *filename;
+
+    if(argc > 1)
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            if(!strncmp(argv[i], "-O0", 3))
+            {
+                optimize_level = 0;
+            }
+            else if(!strncmp(argv[i], "-O1", 3))
+            {
+                optimize_level = 1;
+            }
+            else if(!strncmp(argv[i], "-O2", 3))
+            {
+                optimize_level = 2;
+            }
+            else
+            {
+                filename = strdup(argv[i]);
+            }
+        }
+    }
+    else
+    {
+        printf("Usage: java2pic [-O<optimization level>] filename.java\n");
+        exit(0);
+    }
+
+    char *slash_pos = (char *) rindex(filename, '/') + 1;
+    char *ext_pos = (char *) rindex(filename, '.') + 1;
+    char *outputname = malloc(strlen(slash_pos));
+    assert(outputname != NULL);
+    strncpy(outputname, slash_pos, ext_pos - slash_pos);
+    strcat(outputname, "asm");
+    if(yyin = fopen(filename, "r"))
     {
         bytecode = (ImdtCode *) malloc(sizeof(ImdtCode));
         assert(bytecode != NULL);
@@ -21,7 +59,7 @@ int main(int argc, char *argv[])
     dump_imdt_code(bytecode);
 
     FILE *output;
-    if(output = fopen("output.asm", "w"))
+    if(output = fopen(outputname, "w"))
     {
         codegen_pic(output, bytecode);
         fclose(output);
